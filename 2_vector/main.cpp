030044e9b922458ebd78cfbd6365eff43a4e72c4
abin <abin.z@outlook.com>
2025-01-06 01:00:26 +0800

    feat: 修改2_vector项目文件

--- 2_vector/main.cpp
+++ 2_vector/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 999};
+  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 999, 1000};
   std::cout << "vector<int> v = ";
   for (const auto &item : v)
   {
