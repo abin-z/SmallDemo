316c15d693e62d452c5873911b51dd8c080393c9
abin <abin.z@outlook.com>
2025-01-07 00:01:14 +0800

    feat: 添加注释

--- 1_hello/main.cpp
+++ 1_hello/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int main()
+/// @brief 主函数，程序的入口点
+/// @param argc 命令行参数的数量
+/// @param argv 命令行参数的数组
+/// @return 返回 0 表示程序成功执行
+int main(int argc, char **argv)
 {
   std::cout << "Hello World! I am CMake" << std::endl;
+
   return 0;
 }
