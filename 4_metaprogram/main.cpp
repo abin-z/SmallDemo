3356f08499460bc9b583bb1e5d143319278395bb
abin <abin.z@outlook.com>
2025-01-12 13:58:31 +0800

    fix: 修复enum类型和模板类型不匹配导致的溢出问题

--- 4_metaprogram/main.cpp
+++ 4_metaprogram/main.cpp
@@ -1,42 +1,48 @@
 #include <iostream>
 
-// 模版元编程 - 枚举的方式
-template<long long n>
+// 模版元编程 - 枚举的方式, 注意enum的默认类型是int, 需注意类型不匹配或溢出问题
+template <long long n>
 struct Factorial
 {
-	enum{value = n * Factorial<n - 1>::value};
+  enum : long long
+  {
+    value = n * Factorial<n - 1>::value
+  };
 };
 
-template<>
+template <>
 struct Factorial<0>
 {
-	enum{value = 1};
+  enum : long long
+  {
+    value = 1
+  };
 };
 
 // 模板元编程 - 静态成员变量
-template<long long n>
+template <long long n>
 struct Factor
 {
-	static constexpr  long long value = n * Factor<n-1>::value; 
+  static constexpr long long value = n * Factor<n - 1>::value;
 };
 
-template<>
+template <>
 struct Factor<0> // 终止条件
 {
-	static constexpr  long long value = 1;
+  static constexpr long long value = 1;
 };
 
 int main(const int argc, const char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	std::cout << "hello world" << std::endl;
-	std::cout << "Factorial<5>::value  = " << Factorial<5>::value << std::endl;
-	std::cout << "Factorial<10>::value = " << Factorial<10>::value << std::endl;
-	std::cout << "Factorial<12>::value = " << Factorial<12>::value << std::endl;
-	std::cout << "Factorial<15>::value = " << Factorial<15>::value << std::endl;
-	std::cout << "Factorial<20>::value = " << Factorial<20>::value << std::endl;
+  (void)argc;
+  (void)argv;
+  std::cout << "hello world" << std::endl;
+  std::cout << "Factorial<5>::value  = " << Factorial<5>::value << std::endl;
+  std::cout << "Factorial<10>::value = " << Factorial<10>::value << std::endl;
+  std::cout << "Factorial<12>::value = " << Factorial<12>::value << std::endl;
+  std::cout << "Factorial<15>::value = " << Factorial<15>::value << std::endl;
+  std::cout << "Factorial<20>::value = " << Factorial<20>::value << std::endl;
 
-	std::cout << "Factor<10>::value = " << Factor<10>::value << std::endl;
-	std::cout << "Factor<20>::value = " << Factor<20>::value << std::endl;
+  std::cout << "Factor<10>::value = " << Factor<10>::value << std::endl;
+  std::cout << "Factor<20>::value = " << Factor<20>::value << std::endl;
 }
