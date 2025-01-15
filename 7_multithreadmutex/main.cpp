f295742c522bba4ee3e4fa1edb0e029cbcc19964
zhangbin <zhangbin@yiview.com>
2025-01-15 10:49:02 +0800

    feat: 添加std::atomic使用案例

--- 7_multithreadmutex/main.cpp
+++ 7_multithreadmutex/main.cpp
@@ -1,12 +1,16 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <atomic>
 #include <fmt/core.h>
 
 std::mutex mtx; // 全局互斥量
 int sharedData = 0;
 
+// 原子操作
+std::atomic<int> atomic_sharedData(0);
 
+/// @brief 在多线程下不加锁会出现异常
 void incrementWithoutMutex()
 {
   for (size_t i = 0; i < 100000; ++i)
@@ -15,12 +19,22 @@ void incrementWithoutMutex()
   }
 }
 
+/// @brief 加锁后保障同一时刻只有一个线程修改该共享资源
 void incrementWithMutex()
 {
   for (size_t i = 0; i < 100000; ++i)
   {
     std::lock_guard<std::mutex> locker(mtx); // 自动加锁和解锁
     ++sharedData;
+  }
+}
+
+/// @brief 使用原子整数后, 原子操作就是线程安全的. 不需要锁保护, 适合实现无锁编程 
+void incrementAtomic()
+{
+  for (size_t i = 0; i < 100000; ++i)
+  {
+    atomic_sharedData.fetch_add(3);
   }
 }
 
@@ -36,7 +50,7 @@ int main()
   fmt::println("=======================start========================");
   fmt::println("incrementWithoutMutex sharedData = {}, is incorrect!", sharedData);
 
-  fmt::println("===============================================");
+  fmt::println("--------------------");
   sharedData = 0;
   std::thread tt1(incrementWithMutex);
   std::thread tt2(incrementWithMutex);
@@ -46,6 +60,17 @@ int main()
   tt2.join();
   tt3.join();
   fmt::println("incrementWithMutex sharedData = {}, is correct!", sharedData);
+
+
+  fmt::println("--------------------");
+  std::thread ttt1(incrementAtomic);
+  std::thread ttt2(incrementAtomic);
+  std::thread ttt3(incrementAtomic);
+
+  ttt1.join();
+  ttt2.join();
+  ttt3.join();
+  fmt::println("incrementWithMutex atomic_sharedData = {}, is correct!", atomic_sharedData.load());
   fmt::println("=======================end========================");
   return 0;
 }
