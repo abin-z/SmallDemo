f22674bb04d17460cdd505beeb4c5a6cd967207b
abin <abin.z@outlook.com>
2025-01-22 22:51:41 +0800

    feat: 添加获取硬件支持的线程数

--- 10_readwritelock/main.cpp
+++ 10_readwritelock/main.cpp
@@ -12,7 +12,7 @@
  * 读写锁基本概念：
  * - 共享锁（读锁）：允许多个线程同时读取数据，且不会互相阻塞。
  * - 独占锁（写锁）：只有一个线程可以写入数据，且在写入期间，其他线程不能读也不能写。
- * 
+ *
  * 在 C++14 及更高版本中，std::shared_mutex 是一个很好的选择。它提供了两种基本操作：
  * 共享锁（读锁）：std::shared_lock<std::shared_mutex> lock(rwMutex); 允许多个线程同时读取数据。
  * 独占锁（写锁）：std::unique_lock<std::shared_mutex> lock(rwMutex); 只有一个线程可以写入数据，写入时其他线程不能读或写。
@@ -74,6 +74,8 @@ int main()
     th.join();
   }
   fmt::print("now data : {}\n", sharedData);
+  auto hardware_threads = std::thread::hardware_concurrency();
+  fmt::print("Available hardware threads: {}\n", hardware_threads);
 
   return 0;
 }
