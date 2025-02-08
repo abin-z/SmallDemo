3a3bde6209e4c965560c657a5c813be071156760
abin-z <abin.z@outlook.com>
2025-02-08 17:31:38 +0800

    style: 添加代码注释

--- 13_tuple_pair/main.cpp
+++ 13_tuple_pair/main.cpp
@@ -2,6 +2,16 @@
 #include <string>
 #include <fmt/core.h>
 
+/**
+ * C++ 中，std::tuple、std::pair 和 std::tie 是用来处理多种数据类型组合的工具。它们允许你将多个值捆绑在一起并传递或返回。
+ * 
+ * std::pair：存储两个相关的值，支持不同类型。常用于关联数据结构，例如字典或映射。
+ * std::tuple：可以存储任意数量和类型的值。适用于返回多个不同类型的值或捆绑多个值。
+ * std::tie：解构元组或 pair，将元组中的元素绑定到变量上。非常适用于多返回值的场景。
+ * 这些工具在 C++ 中提供了灵活的数据组织和返回机制，尤其在处理函数返回多个值、存储复合数据时非常有用。
+ * 
+ */
+
 void testPair()
 {
   std::pair<int, std::string> pair = std::make_pair(3, "hello world!");
