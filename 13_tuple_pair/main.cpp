717e9b56b988ee3b93cb9f9cf30f793810a156a0
abin-z <abin.z@outlook.com>
2026-04-15 11:03:03 +0800

    feat add pair test

--- 13_tuple_pair/main.cpp
+++ 13_tuple_pair/main.cpp
@@ -1,11 +1,13 @@
-#include <utility>
-#include <string>
-#include <typeinfo>
-#include <tuple>
 #include <fmt/core.h>
 
+#include <string>
+#include <tuple>
+#include <typeinfo>
+#include <utility>
+
 /**
- * C++ 中，std::tuple、std::pair 和 std::tie 是用来处理多种数据类型组合的工具。它们允许你将多个值捆绑在一起并传递或返回。
+ * C++ 中，std::tuple、std::pair 和 std::tie
+ * 是用来处理多种数据类型组合的工具。它们允许你将多个值捆绑在一起并传递或返回。
  *
  * std::pair：存储两个相关的值，支持不同类型。常用于关联数据结构，例如字典或映射。
  * std::tuple：可以存储任意数量和类型的值。适用于返回多个不同类型的值或捆绑多个值。
@@ -18,6 +20,10 @@ void testPair()
 {
   std::pair<int, std::string> pair = std::make_pair(3, "hello world!");
   fmt::print("pair.first = {}, pair.second = {}\n", pair.first, pair.second);
+  auto pair2 = std::make_pair(42, "C++");
+  auto [x, y] = pair2;
+  fmt::print("pair2.first = {}, pair2.second = {}\n", pair2.first, pair2.second);
+  fmt::print("x = {}, y = {}\n", x, y);
 }
 
 void testTuple()
@@ -73,18 +79,24 @@ struct point
 
 void testTuple02()
 {
-  using funcType = std::tuple<int, std::string> (*)(); // 函数指针类型
+  using funcType = std::tuple<int, std::string> (*)();  // 函数指针类型
   std::tuple<int, std::string, double, point, int64_t, funcType> tp{32, "Apple", 4.5, {1, 2, 3}, 99999, getData};
   // std::tuple_size 用于获取 std::tuple 类型中元素的数量。它是一个类型特征，返回一个表示元组元素数量的常量。
   size_t size = std::tuple_size<decltype(tp)>::value;
   fmt::print("std::tuple_size<decltype(tp)>::value = {}\n", size);
   // std::tuple_element 用于获取 std::tuple 中指定索引位置的元素类型。它是一个类型特征，返回元组中指定位置的元素类型。
-  fmt::println("std::tuple_element<0, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<0, decltype(tp)>::type).name());
-  fmt::println("std::tuple_element<1, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<1, decltype(tp)>::type).name());
-  fmt::println("std::tuple_element<2, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<2, decltype(tp)>::type).name());
-  fmt::println("std::tuple_element<3, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<3, decltype(tp)>::type).name());
-  fmt::println("std::tuple_element<4, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<4, decltype(tp)>::type).name());
-  fmt::println("std::tuple_element<5, decltype(tp)>::type).name() = {}", typeid(std::tuple_element<5, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<0, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<0, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<1, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<1, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<2, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<2, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<3, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<3, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<4, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<4, decltype(tp)>::type).name());
+  fmt::println("std::tuple_element<5, decltype(tp)>::type).name() = {}",
+               typeid(std::tuple_element<5, decltype(tp)>::type).name());
 }
 
 int main()
