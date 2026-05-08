5034b9e9b7d13f305ca7b70574e0671abd0e5cc5
abin <abin.z@outlook.com>
2026-05-08 23:45:29 +0800

    perf: 优化锁专题

--- 11_locktopic/main.cpp
+++ 11_locktopic/main.cpp
@@ -1,34 +1,37 @@
-#include <thread>
-#include <mutex>
-#include <shared_mutex>
-#include <vector>
-#include <utility>
-#include <chrono>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+
+#include <mutex>
+#include <shared_mutex>
 #include <sstream>
+#include <thread>
+#include <vector>
 
 /*
  * C++ 锁机制专题
  *   互斥锁 (std::mutex) 是 C++11 引入的基础锁类型，用来保证多个线程访问共享资源时的互斥性
- *   递归锁 (std::recursive_mutex) 是 std::mutex 的一种变体，它允许同一个线程多次锁定同一互斥量，而不会发生死锁。但对于 不同线程，它和普通的互斥锁一样，会表现为互斥行为
- *   共享锁与独占锁 (std::shared_mutex) 是一种新的锁机制，允许多个线程以“共享”方式并发读共享资源，而写线程则必须获得独占锁，阻塞其他读线程和写线程。
- *           共享锁: std::shared_lock<std::shared_mutex> lock(rwMutex);  // 共享锁
- *           独占锁: std::unique_lock<std::shared_mutex> lock(rwMutex);  // 独占锁
+ *   递归锁 (std::recursive_mutex) 是 std::mutex 的一种变体，它允许同一个线程多次锁定同一互斥量，而不会发生死锁。但对于
+ * 不同线程，它和普通的互斥锁一样，会表现为互斥行为 共享锁与独占锁 (std::shared_mutex)
+ * 是一种新的锁机制，允许多个线程以“共享”方式并发读共享资源，而写线程则必须获得独占锁，阻塞其他读线程和写线程。 共享锁:
+ * std::shared_lock<std::shared_mutex> lock(rwMutex);  // 共享锁 独占锁: std::unique_lock<std::shared_mutex>
+ * lock(rwMutex);  // 独占锁
  *
  * 防止死锁的方法：
  *   固定锁的顺序：让多个线程按照固定顺序来获取多个锁。
  *   避免长时间持有锁：避免长时间持有锁的操作，缩小锁的作用范围。
  *   使用 std::lock：std::lock 可以同时锁定多个互斥量，避免死锁。
- * 
- *   std::lock 用于同时锁定多个互斥量。它会尝试以相同的顺序获取所有互斥量。如果某个互斥量已经被锁定，它会阻塞直到所有锁都成功获取
+ *
+ *   std::lock
+ * 用于同时锁定多个互斥量。它会尝试以相同的顺序获取所有互斥量。如果某个互斥量已经被锁定，它会阻塞直到所有锁都成功获取
  *   std::lock 本身不会管理锁的生命周期，它只是确保多个互斥量不会发生死锁。
- *   你仍然需要使用 std::lock_guard 或其他 RAII 风格的锁来自动释放锁，否则锁会保持在任务完成后仍然处于锁定状态，这会导致其他线程无法获取这些锁，从而发生死锁。
+ *   你仍然需要使用 std::lock_guard 或其他 RAII
+ * 风格的锁来自动释放锁，否则锁会保持在任务完成后仍然处于锁定状态，这会导致其他线程无法获取这些锁，从而发生死锁。
  *   std::lock(mtx1, mtx2, ...);  // 尝试同时锁定 mtx1 和 mtx2
  *   std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx1
  *   std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx2
- *   
- *   范围锁 (std::scoped_lock) 是 C++17 引入的一个轻量级 RAII 锁管理类，它的主要特点是可以同时锁多个互斥量并防止死锁，适用于简单且高效的场景。
+ *
+ *   范围锁 (std::scoped_lock) 是 C++17 引入的一个轻量级 RAII
+ * 锁管理类，它的主要特点是可以同时锁多个互斥量并防止死锁，适用于简单且高效的场景。
  *   - 构造时自动加锁;
  *   - 析构时自动解锁;
  *   - 支持多个互斥量一次性加锁，内部自动使用 std::lock 避免死锁;
@@ -46,12 +49,12 @@ size_t tid()
 
 /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /// @brief 互斥锁
-std::mutex mtx; // 互斥量
+std::mutex mtx;  // 互斥量
 int sharedData = 0;
 void increment()
 {
-  std::lock_guard<std::mutex> locker(mtx); // 互斥锁,自动锁定和释放
-  ++sharedData;                            // 操作共享资源
+  std::lock_guard<std::mutex> locker(mtx);  // 互斥锁,自动锁定和释放
+  ++sharedData;                             // 操作共享资源
 }
 void testMutex()
 {
@@ -65,17 +68,17 @@ void testMutex()
 
 /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /// @brief 递归锁
-std::recursive_mutex rmtx; // 递归互斥量
+std::recursive_mutex rmtx;  // 递归互斥量
 int counter = 0;
 void recursiveFunction(int depth)
 {
-  std::lock_guard<std::recursive_mutex> locker(rmtx); // 递归锁: 允许同一个线程多次锁定同一互斥量，而不会发生死锁。
+  std::lock_guard<std::recursive_mutex> locker(rmtx);  // 递归锁: 允许同一个线程多次锁定同一互斥量，而不会发生死锁。
   if (depth > 0)
   {
     fmt::print("Recursion depth: {}\n", depth);
     recursiveFunction(depth - 1);
   }
-  ++counter; // 操作共享资源
+  ++counter;  // 操作共享资源
 }
 void testRecursiveMutex()
 {
@@ -87,31 +90,33 @@ void testRecursiveMutex()
 /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /// @brief 共享锁
 std::shared_mutex rwMtx;
-std::vector<int> sharedDataVec; // 共享资源
+std::vector<int> sharedDataVec;  // 共享资源
 void readData()
 {
-  std::shared_lock<std::shared_mutex> locker(rwMtx); // 读共享
+  std::shared_lock<std::shared_mutex> locker(rwMtx);  // 读共享
   fmt::print("[{:#06x}]: read data {}\n", tid(), sharedDataVec);
 }
 void writeData(int value)
 {
-  std::unique_lock<std::shared_mutex> locker(rwMtx); // 写独占
+  std::unique_lock<std::shared_mutex> locker(rwMtx);  // 写独占
   sharedDataVec.push_back(value);
   fmt::print("[{:#06x}]: write value {}\n", tid(), value);
 }
 void testSharedMutex()
 {
-  std::vector<std::thread> writers; // 写线程
+  std::vector<std::thread> writers;  // 写线程
   constexpr int writerCount = 3;
+  writers.reserve(writerCount);
   for (int i = 0; i < writerCount; ++i)
   {
-    writers.emplace_back(std::thread(writeData, i * 10));
+    writers.emplace_back(writeData, i * 10);
   }
-  std::vector<std::thread> readers; // 读取线程
+  std::vector<std::thread> readers;  // 读取线程
   constexpr int readerCount = 5;
+  readers.reserve(readerCount);
   for (int i = 0; i < readerCount; ++i)
   {
-    readers.emplace_back(std::thread(readData));
+    readers.emplace_back(readData);
   }
   for (auto &th : writers)
   {
@@ -128,16 +133,16 @@ void testSharedMutex()
 std::mutex mtx1, mtx2;
 void task1()
 {
-  std::lock(mtx1, mtx2);                                    // 尝试同时锁定 mtx1 和 mtx2, 参数顺序不重要
-  std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock); // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx1
-  std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock); // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx2
+  std::lock(mtx1, mtx2);                                     // 尝试同时锁定 mtx1 和 mtx2, 参数顺序不重要
+  std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx1
+  std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx2
   fmt::print("Task 1 completed\n");
 }
 void task2()
 {
-  std::lock(mtx2, mtx1);                                    // 尝试同时锁定 mtx1 和 mtx2
-  std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock); // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx1
-  std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock); // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx2
+  std::lock(mtx2, mtx1);                                     // 尝试同时锁定 mtx1 和 mtx2
+  std::lock_guard<std::mutex> lock1(mtx1, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx1
+  std::lock_guard<std::mutex> lock2(mtx2, std::adopt_lock);  // 使用 adopt_lock 告诉 lock_guard 不要再尝试锁定 mtx2
   fmt::print("Task 2 completed\n");
 }
 void testVoidDeadlock()
